b88767d4ab07c236ad7ab69d7bf67c75c4caad94
karan078 <82096487+karan078@users.noreply.github.com>
2024-01-10 18:13:05 +0530

    Add files via upload

---
+++ task2/App.js
@@ -0,0 +1,56 @@
+import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Routes,
+  Link
+} from 'react-router-dom'
+import Nav from './Component/Nav'
+import DetailsFill from './Component/DetailsFill';
+import Home from './Component/Home'
+import Table from './Component/Table'
+import BloodData from './Component/BloodData';
+import LogOut from './Component/LogOut'
+// import ApiDataTable from ''
+// import detailFill from './Component/detailFill'
+// import './Component/navbar.js'
+// when we do an npm start this particular section will resolve to what we view over the web
+// it's a complete site
+
+function App() {
+  return (
+    // return only one not more than one
+    // jsx fragment
+    // done
+    <> 
+      <div className="my_navigation">
+        <Router>
+          <Nav/>
+            <Switch>
+              <Route exact path='/'>
+                <Home/>
+              </Route>
+              <Route path="/DetailsFill">
+                <DetailsFill/>
+              </Route>
+              <Route path="/Table">
+                <Table/>
+              </Route>
+              <Route path="/ApiDataTable">
+                <Table/>
+              </Route>
+              <Route path = "/blooddata">
+                <BloodData/>
+              </Route>
+            </Switch>
+        </Router>
+      {/* <LogOut></LogOut> */}
+      </div>
+    </>
+      // <h1 className="my_name">Hello everyone</h1>
+  );
+}
+
+export default App;
